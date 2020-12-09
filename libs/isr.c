5ee46f70bb27d19ea3ecf02cd80627c6fede122c
Pablo Garrido <pablogs9@gmail.com>
2020-12-09 19:11:06 +0100

    Add ISR debouncing

--- libs/isr.c
+++ libs/isr.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 
 #include "isr.h"
+#include "timer.h"
 
 isr_struct_t isr_struct_array[NUM_ISR_SOURCES] = {0};
 
@@ -12,9 +13,13 @@ isr_struct_t isr_struct_array[NUM_ISR_SOURCES] = {0};
 void  isr_irq(void){
 	uint32_t current_vector = irq_vector;
 
-	for (size_t i = 0; i < NUM_ISR_SOURCES && current_vector; i++, current_vector >>= 1){	
-		if ((current_vector & 0b1 ) && isr_struct_array[i].callback != NULL){
+	for (size_t i = 0; i < NUM_ISR_SOURCES && current_vector; i++, current_vector >>= 1){			
+		if ((current_vector & 0b1 ) && 
+			isr_struct_array[i].callback != NULL &&
+			(isr_struct_array[i].debounce == 0 || *timers[0].addr > isr_struct_array[i].next_time)){
+
 			isr_struct_array[i].callback(isr_struct_array[i].args);
+			isr_struct_array[i].next_time = *timers[0].addr + isr_struct_array[i].debounce;
 		}
 	}
 
--- libs/isr.h
+++ libs/isr.h
@@ -6,6 +6,9 @@
 	X(TIMER1) 		\
 	X(UART0) 		\
 	X(BUTTON1) 		\
+	X(BUTTON2) 		\
+	X(BUTTON3) 		\
+	X(BUTTON4) 		\
 
 typedef void (*isr_handler)(void *);
 
@@ -20,6 +23,8 @@ enum
 typedef struct isr_struct_t {
     isr_handler callback;
     void * args;
+	uint32_t debounce;
+	uint32_t next_time;
 } isr_struct_t;
 
 extern isr_struct_t isr_struct_array[NUM_ISR_SOURCES];
@@ -27,8 +32,10 @@ extern isr_struct_t isr_struct_array[NUM_ISR_SOURCES];
 #define irq_vector (*(volatile uint32_t*) 0x09000000)
 #define irq_vector_mask (*(volatile uint32_t*) 0x09000004)
 
-#define set_isr_callback(isr, func) do {isr_struct_array[isr].callback = func; irq_vector_mask |= 1UL << isr;} while(0);
-#define set_isr_callback_with_args(isr, func, args_in) do {isr_struct_array[isr].callback = func; isr_struct_array[isr].args = args_in; irq_vector_mask |= 1UL << isr;} while(0);
+#define set_isr_callback(isr, func) do {isr_struct_array[isr].callback = func; isr_struct_array[isr].debounce = 0; irq_vector_mask |= 1UL << isr;} while(0);
+#define set_isr_callback_with_args(isr, func, args_in) do {isr_struct_array[isr].callback = func; isr_struct_array[isr].debounce = 0; isr_struct_array[isr].args = args_in; irq_vector_mask |= 1UL << isr;} while(0);
+#define set_isr_callback_with_debounce(isr, func, deb) do {isr_struct_array[isr].callback = func; isr_struct_array[isr].debounce = deb; isr_struct_array[isr].next_time = *timers[0].addr + deb; irq_vector_mask |= 1UL << isr;} while(0);
+
 #define unset_isr_callback(isr) do {isr_struct_array[isr].callback = NULL; irq_vector_mask &= ~(1UL << isr);} while(0);
 
 
